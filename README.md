c96d3c4e121c4fad911007469ec4f052cc7ee852
Leonidas-I <morningstar8x@outlook.com>
2020-08-14 09:39:27 +0700

    UI is done with RWD + add README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+# WTF is this
+
+Landing page use only html, css, js and nothing else
+
+# Things i have done
+
+css-grid, scss  
+webpack for bundle with seperate development and product mode  
+npm script and package
+
+# Things i may have improved
+
+PWA
--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-///* Declare Global Variable *///
 const getElement = (e) => document.querySelectorAll(e);
-let slideIndex = 1;
-const slides = getElement(".slideshow__image");
-const dots = getElement(".slideshow__dot");
 
 ///* Curtain Menu *///
 getElement(".hamburger")[0].addEventListener("click", () => {
@@ -13,8 +9,36 @@ getElement(".back")[0].addEventListener("click", () => {
     getElement(".nav__list")[0].classList.remove("open");
 });
 
-///* Navigate SlideShow *///
+getElement(".nav__list")[0].addEventListener("click", () => {
+    getElement(".nav__list")[0].classList.remove("open");
+});
+
+///* Navigate SlideShow on Arrow-Click*///
+getElement(".slideshow__arrow--prev")[0].addEventListener("click", () => {
+    goSlide__arrowClick(-1);
+});
+
+getElement(".slideshow__arrow--next")[0].addEventListener("click", () => {
+    goSlide__arrowClick(1);
+});
+
+const goSlide__arrowClick = (n) => showSlide((slideIndex += n));
+
+/* Navigate SlideShow With Event Delegation on Dot-click */
+getElement(".slideshow__indicator")[0].addEventListener("click", (e) => {
+    for (let i = 0; i < dots.length; i++) {
+        if (dots[i] === e.target) {
+            showSlide(i + 1);
+        }
+    }
+});
+
+let slideIndex = 1;
+const slides = getElement(".slideshow__image");
+const dots = getElement(".slideshow__dot");
+
 const showSlide = (n) => {
+    /* Ternary Operator */
     n > slides.length
         ? (slideIndex = 1)
         : n < 1
@@ -28,22 +52,4 @@ const showSlide = (n) => {
     dots[slideIndex - 1].style.opacity = "1";
 };
 
-const goSlide__arrowClick = (n) => showSlide((slideIndex += n));
-
 showSlide(slideIndex);
-
-getElement(".slideshow__arrow--prev")[0].addEventListener("click", () => {
-    goSlide__arrowClick(-1);
-});
-
-getElement(".slideshow__arrow--next")[0].addEventListener("click", () => {
-    goSlide__arrowClick(1);
-});
-
-getElement(".slideshow__indicator")[0].addEventListener("click", (e) => {
-    for (let i = 0; i < dots.length; i++) {
-        if (dots[i] === e.target) {
-            showSlide(i + 1);
-        }
-    }
-});
